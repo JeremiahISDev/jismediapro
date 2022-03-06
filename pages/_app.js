6b0bd4ca2efa22b9a573915245ba7b7fa105b38e
Jeremiah Ihab Samuel <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2022-03-06 16:48:28 +0000

    add crisp

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import Head from 'next/head'
+import Script from "next/script"
 import Navigation from '../components/Navigation'
 export default function Portfolio({ Component, pageProps }) {
   return (
@@ -8,6 +9,22 @@ export default function Portfolio({ Component, pageProps }) {
         
        <title>JIS Media Pro</title>
       </Head>
+		<Script
+  id="crisp-widget"
+  strategy="afterInteractive"
+  dangerouslySetInnerHTML={{
+    __html: `
+      window.$crisp=[];
+      window.CRISP_WEBSITE_ID="9f460efd-73d6-4fd2-af0f-11d2591c0e7c";
+      (function(){
+        const d = document;
+        const s = d.createElement("script");
+        s.src = "https://client.crisp.chat/l.js";
+        s.async = 1;
+        d.getElementsByTagName("head")[0].appendChild(s);
+      })();`,
+  }}
+/>
       <Navigation />
       <Component {...pageProps} />
     </>
