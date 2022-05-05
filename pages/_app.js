a31e0afa71dbb6542163d81eaaa97254889295fc
JeremiahISamuelMMS <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2022-05-05 18:35:24 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -15,6 +15,18 @@ export default function Portfolio({ Component, pageProps }) {
     __html: `window.$crisp=[];window.CRISP_WEBSITE_ID="b63678f6-e71f-4f61-a902-3d322733893e";(function(){d=document;s=d.createElement("script");s.src="https://client.crisp.chat/l.js";s.async=1;d.getElementsByTagName("head")[0].appendChild(s);})();`,
   }}
 />
+
+<Script
+  id="smartlook"
+  dangerouslySetInnerHTML={{
+    __html: `  window.smartlook||(function(d) {
+    var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+    var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+    c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+    })(document);
+    smartlook('init', 'a47f66c73623b0ad1225a7b9714c1d8fcd87c963', { region: 'eu' });`,
+  }}
+/>
       <Navigation />
       <Component {...pageProps} />
     </>
