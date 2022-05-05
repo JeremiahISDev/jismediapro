4b11ee280fea5c2240e697199a54736549eac7bb
JeremiahISamuelMMS <79987177+JeremiahISamuelMMS@users.noreply.github.com>
2022-05-05 17:41:16 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -12,7 +12,20 @@ export default function Portfolio({ Component, pageProps }) {
 		<Script
   id="chat-widget"
   dangerouslySetInnerHTML={{
-    __html: `function initFreshChat() {     window.fcWidget.init({       token: "91a3199a-6765-42fb-b823-f7cc88711c67",       host: "https://wchat.freshchat.com"     });   }   function initialize(i,t){var e;i.getElementById(t)?initFreshChat():((e=i.createElement("script")).id=t,e.async=!0,e.src="https://wchat.freshchat.com/js/widget.js",e.onload=initFreshChat,i.head.appendChild(e))}function initiateCall(){initialize(document,"Freshdesk Messaging-js-sdk")}window.addEventListener?window.addEventListener("load",initiateCall,!1):window.attachEvent("load",initiateCall,!1);`,
+    __html: `(function(d,t) {
+        var BASE_URL="https://app.chatwoot.com";
+        var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src=BASE_URL+"/packs/js/sdk.js";
+        g.defer = true;
+        g.async = true;
+        s.parentNode.insertBefore(g,s);
+        g.onload=function(){
+          window.chatwootSDK.run({
+            websiteToken: 'f4GLk9iMJBENDF8yPM6EmhVi',
+            baseUrl: BASE_URL
+          })
+        }
+      })(document,"script");`,
   }}
 />
       <Navigation />
